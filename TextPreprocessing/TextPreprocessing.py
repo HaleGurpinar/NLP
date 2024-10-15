7414ad3c73ba473a21d76be942f37c3c2599083f
Hale <itshalenurgurpinar@gmail.com>
2024-10-15 19:33:39 +0300

    text P

--- TextPreprocessing/TextPreprocessing.py
+++ TextPreprocessing/TextPreprocessing.py
@@ -12,3 +12,13 @@
 from warnings import filterwarnings
 import pandas as pd
 
+filterwarnings('ignore')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 200)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+# ###########################################################
+# 1. Text Preprocessing
+
+df = pd.read_csv("amazon_reviews.csv", sep=",")
+df.head(5)
